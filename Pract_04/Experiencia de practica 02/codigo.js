007c1c7810266f98a1ffbaf4f91bde36ba6cc5c2
HectorDaniell <hector.oviedo@ucsm.edu.pe>
2022-09-13 00:43:49 -0500

    Experiencia de practica 02

---
+++ Pract_04/Experiencia de practica 02/codigo.js
@@ -0,0 +1,51 @@
+ //PARTE A
+ /*
+function funcion1(numero){
+
+    var varDeBloque = 'Daniel';
+    const constDeBloque=13;
+  
+}    
+
+console.log(varDeBloque);
+console.log(constDeBloque);
+*/
+
+// PARTE B Y C
+/*
+console.log(x === undefined); // true 
+var x=3;
+// devolverá un valor de undefined 
+var myvar = 'my value';
+(function funcion2() {
+    console.log(myVar); // undefined 
+    var myvar = 'valor local';
+})();
+console.log(myvar);
+console.log(x);
+*/
+
+//PARTE D
+/*
+var answer = 42;
+answer = 'Gracias por todo el pescado...';
+*/
+/*
+parseInt("F", 16); 
+parseInt("17", 8); 
+parseInt("15", 10); 
+parseInt(15.99, 10);
+parseInt("FXX123", 16); 
+parseInt("1111", 2); 
+parseInt("15*3", 10); 
+parseInt("12", 13);
+parseInt("Hello", 8); // No es un número en absoluto
+parseInt("0x7", 10); // No es de base 10
+parseInt("546",2); //Losdígitosnosonválidospararepresentacionesbinarias.
+*/
+
+var howMany = 10;
+alert("howMany.toString() is " + howMany.toString()); // displays "10" 
+alert("45 .toString() is " + 45 .toString()); //displays "45"
+varx=7;
+alert(x.toString(2)) // Displays "111"

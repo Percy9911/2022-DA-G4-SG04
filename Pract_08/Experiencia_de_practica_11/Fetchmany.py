7f66927e2c2951b92c6ea545a7618546a03db211
Percy Mariano Choquehuanca Luna <102229594+Percy9911@users.noreply.github.com>
2022-10-17 20:01:37 -0500

    Delete Pract_08/Experiencia_de_practica_11 directory

--- Pract_08/Experiencia_de_practica_11/Fetchmany.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('orders.db')
-print("Base de datos abierta satisfactoriamente");
-cur = conn.cursor()
-cur.execute("SELECT * FROM orders;")
-five_results = cur.fetchmany(5)
-print(five_results)
-print("Consulta realizada satisfactoriamente");
-conn.close()

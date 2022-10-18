166c5aa30af8f9597793fd12fd4ff03730a80172
Percy Mariano Choquehuanca Luna <102229594+Percy9911@users.noreply.github.com>
2022-10-18 08:49:50 -0500

    Add files via upload

---
+++ Pract_08/Experiencia de practica 08/Constructores.py
@@ -0,0 +1,9 @@
+class Curso():
+    nombre=" "
+    codigo=" "
+def __init__(self):
+    self.nombre="Matematica"
+    self.codigo="24142"
+def __del__(self):
+    print("Objeto borrado")
+curso1=Curso()
